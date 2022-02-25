b07ca2ea695dd3de54d763b5e0ffe124797b0690
robss9419 <56467911+robss9419@users.noreply.github.com>
2022-02-25 14:14:51 +0100

    Update 4.java

--- 4.java
+++ 4.java
@@ -1,14 +1,3 @@
-Napisz program, który pobierze od użytkownika macierz sąsiedztwa grafu G (wierzchołki numerujemy od jedynki), a następnie
-wyświetli następujące informacje (każda informacja w osobnej linii):
-
-    Ilość wierzchołków
-    Ilość krawędzi
-    Ciąg stopni (nieposortowane)
-    Średni stopień grafu
-    Informacja czy graf jest pełny/cykl/ścieżka/drzewo/lub nie należy do żadnych
-
-Sprawdzamy w podanej powyżej kolejności i wypisujemy każdy poprawny wynik. Zakładamy dla prostoty, że grafy są spójne, ale dopuszczamy wierzchołki izolowane.
-
 import java.util.*;
 
 class Main {
